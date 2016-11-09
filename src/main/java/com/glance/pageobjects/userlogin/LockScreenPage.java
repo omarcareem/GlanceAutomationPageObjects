5fe0da50a29e139ee8084e44666a398603352849
Abdul Kareem <Abkareem@virtusapolaris.com>
2016-11-09 14:10:01 +0530

    Added new java class

---
+++ src/main/java/com/glance/pageobjects/userlogin/LockScreenPage.java
@@ -0,0 +1,14 @@
+package com.glance.pageobjects.userlogin;
+
+import org.openqa.selenium.WebDriver;
+
+import com.glance.pageobjects.common.BasePage;
+
+public class LockScreenPage extends BasePage{
+
+	public LockScreenPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}

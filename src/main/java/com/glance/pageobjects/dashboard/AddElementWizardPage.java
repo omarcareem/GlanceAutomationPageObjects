2c7c93df3b0a002243d0c27f7f593689534bc132
Abdul Kareem <Abkareem@virtusapolaris.com>
2016-11-09 11:37:34 +0530

    added new page object classes

---
+++ src/main/java/com/glance/pageobjects/dashboard/AddElementWizardPage.java
@@ -0,0 +1,14 @@
+package com.glance.pageobjects.dashboard;
+
+import org.openqa.selenium.WebDriver;
+
+import com.glance.pageobjects.common.BasePage;
+
+public class AddElementWizardPage extends BasePage{
+
+	public AddElementWizardPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/glance/pageobjects/datamanagement/DataUploadPage.java
@@ -0,0 +1,14 @@
+package com.glance.pageobjects.datamanagement;
+
+import org.openqa.selenium.WebDriver;
+
+import com.glance.pageobjects.common.BasePage;
+
+public class DataUploadPage extends BasePage{
+
+	public DataUploadPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}

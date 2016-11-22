ae9f2a388f97671e76a0da1a7fc47e4babfb35d6
jananinirasha <jananinirasha@gmail.com>
2016-11-22 12:07:24 +0530

    added updates 22/11/2016

--- src/main/java/com/glance/pageobjects/common/CommonPageObject.java
+++ src/main/java/com/glance/pageobjects/common/CommonPageObject.java
@@ -9,15 +9,19 @@ package com.glance.pageobjects.common;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.glance.pageobjects.logs.TestLog;
@@ -27,6 +31,7 @@ public class CommonPageObject extends BasePage{
 	public CommonPageObject(WebDriver driver) {
 		super(driver);
 		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver, this);
 	}
 	
 	
@@ -42,43 +47,257 @@ public class CommonPageObject extends BasePage{
 	@FindBy(id="example_last")
 	WebElement btnLast;
 	
-	//Page Navigation - First page
-		public void FirstPageNavigation(){
+	@FindBy(name="example_length")
+	WebElement dropDown;
+	
+	@FindBy(id="example_info")
+	WebElement lblRecordCount;
+	
+	By middlePagination = By.xpath("//div[@id='example_paginate']/span/a");
+
+	
+	
+		//Page Navigation - First page
+		public void firstPageNavigation(){
+			
+			try{
+			TestLog.log.info("Check First Button eneble");	
 			if(btnFirst.isEnabled()){
+				TestLog.log.info("Click on First Button");	
 				btnFirst.click();
 			}else{
-				System.out.println("Page Navigation button Disable");
+				TestLog.log.info("Page Navigation button First Disable");
+			}
+			} catch (Exception ex) {
+				TestLog.log.info("Could not find page Navigation button" + ex);
 			}
 		}
 		
 		
-	//Page Navigation - Previous page
+		//Page Navigation - Previous page
 		public void previousPageNavigation(){
-			if(btnPrevious.isEnabled()){
-				btnPrevious.click();
-			}else{
-				System.out.println("Page Navigation button Disable");
-			}
+			try{
+				TestLog.log.info("Check Previous Button eneble");	
+				if(btnPrevious.isEnabled()){
+					TestLog.log.info("Click on Previous Button");	
+					btnPrevious.click();
+				}else{
+					TestLog.log.info("Page Navigation button Previous Disable");
+				}
+				} catch (Exception ex) {
+					TestLog.log.info("Could not find page Navigation button" + ex);
+				}
 		}
 
-	//Page Navigation - Next page
-		public void NextPageNavigation(){
-			if(btnNext.isEnabled()){
-				btnNext.click();
-			}else{
-				System.out.println("Page Navigation button Disable");
+		//Page Navigation - Next page
+		public void nextPageNavigation(){
+			try{
+				TestLog.log.info("Check Next Button eneble");	
+				if(btnNext.isEnabled()){
+					TestLog.log.info("Click on Next Button");	
+					btnNext.click();
+				}else{
+					TestLog.log.info("Page Navigation button Previous Disable");
+				}
+				} catch (Exception ex) {
+					TestLog.log.info("Could not find page Navigation button" + ex);
 				}
 		}
 			
-	//Page Navigation - Last page
-		public void LPageNavigation(){
-			if(btnLast.isEnabled()){
-				btnLast.click();
-			}else{
-				System.out.println("Page Navigation button Disable");
-			}
+		//Page Navigation - Last page
+		public void lastPageNavigation(){
+			try{
+				TestLog.log.info("Check Last Button eneble");	
+				if(btnLast.isEnabled()){
+					TestLog.log.info("Click on Last Button");	
+					btnLast.click();
+				}else{
+					TestLog.log.info("Page Navigation button Previous Disable");
+				}
+				} catch (Exception ex) {
+					TestLog.log.info("Could not find page Navigation button" + ex);
+				}
 		}
 		
+		//Page navigation - Middle Pages
+		
+		public void middlePageNavigation(){
+			try{
+				List<WebElement> pagination= driver.findElements(middlePagination);
+				TestLog.log.info("checkif pagination link exists");	
+				
+				if(pagination .size()>0){ 
+					System.out.println("pagination exists");
+					TestLog.log.info("click on pagination link ");
+					for(int i=0; i<pagination .size(); i++){ 
+						pagination.get(i).click(); 
+						} 
+						} else { 
+						System.out.println("pagination not exists");
+						} 
+				} catch (Exception ex) {
+					TestLog.log.info("Could not find page Navigation button" + ex);
+				}
+		}
+		
+		//select the table entry count from the drop down
+		public void selectDropDuwn(String numberOfRecords){
+			try{
+				System.out.println("check dropdown exist");
+				if (dropDown.isDisplayed()){
+					System.out.println("select from dropdown list");
+					Select drpRecords= new Select(dropDown);
+					drpRecords.selectByVisibleText(numberOfRecords);
+				}else{
+					System.out.println("dropdown does not exist");
+					System.out.println("Table Entry dropdown not visible");
+				}
+				
+			} catch (Exception ex) {
+				TestLog.log.info("Could not find dropdown" + ex);
+			}
+			
+		}
+		
+		//Page navigation verification
+		public void verifyDroDownn(){
+			try{
+				System.out.println("verify selection");
+				
+				Select visibleRecords= new Select(dropDown);
+				
+				System.out.println("get table row count");
+				WebElement element = visibleRecords.getFirstSelectedOption();
+				String selectedValue= element.getText();
+				int rawcount= driver.findElements(By.tagName("tr")).size(); 
+				
+				System.out.println("get page number count");
+				int pageNumbers = driver.findElements(By.xpath("//span/a")).size();
+				int pageCount;
+								
+				System.out.println("get page count");
+				if (rawcount < 10){
+					if(selectedValue=="10"){
+						System.out.println("get page count");
+						pageCount= (rawcount/10);
+						System.out.println(pageCount);						
+							} else if(selectedValue=="25"){
+								System.out.println("get page count");
+								pageCount= (rawcount/25);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="50"){
+								System.out.println("get page count");
+								pageCount= (rawcount/50);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="100"){
+								System.out.println("get page count");
+								pageCount= (rawcount/100);
+								System.out.println(pageCount);						
+							}else{
+								System.out.println("No pages appeared");
+							}
+				}
+				
+				if (rawcount < 25){
+					if(selectedValue=="10"){
+						System.out.println("get page count");
+						pageCount= (rawcount/10);
+						System.out.println(pageCount);						
+							} else if(selectedValue=="25"){
+								System.out.println("get page count");
+								pageCount= (rawcount/25);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="50"){
+								System.out.println("get page count");
+								pageCount= (rawcount/50);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="100"){
+								System.out.println("get page count");
+								pageCount= (rawcount/100);
+								System.out.println(pageCount);						
+							}else{
+								System.out.println("No pages appeared");
+							}
+				}
+				
+				if (rawcount < 50){
+					if(selectedValue=="10"){
+						System.out.println("get page count");
+						pageCount= (rawcount/10);
+						System.out.println(pageCount);						
+							} else if(selectedValue=="25"){
+								System.out.println("get page count");
+								pageCount= (rawcount/25);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="50"){
+								System.out.println("get page count");
+								pageCount= (rawcount/50);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="100"){
+								System.out.println("get page count");
+								pageCount= (rawcount/100);
+								System.out.println(pageCount);						
+							}else{
+								System.out.println("No pages appeared");
+							}
+				}
+				
+				if (rawcount < 100){
+					if(selectedValue=="10"){
+						System.out.println("get page count");
+						pageCount= (rawcount/10);
+						System.out.println(pageCount);						
+							} else if(selectedValue=="25"){
+								System.out.println("get page count");
+								pageCount= (rawcount/25);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="50"){
+								System.out.println("get page count");
+								pageCount= (rawcount/50);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="100"){
+								System.out.println("get page count");
+								pageCount= (rawcount/100);
+								System.out.println(pageCount);						
+							}else{
+								System.out.println("No pages appeared");
+							}
+				}
+				
+				if (rawcount > 100){
+					if(selectedValue=="10"){
+						System.out.println("get page count");
+						pageCount= (rawcount/10);
+						System.out.println(pageCount);						
+							} else if(selectedValue=="25"){
+								System.out.println("get page count");
+								pageCount= (rawcount/25);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="50"){
+								System.out.println("get page count");
+								pageCount= (rawcount/50);
+								System.out.println(pageCount);						
+							}else if(selectedValue=="100"){
+								System.out.println("get page count");
+								pageCount= (rawcount/100);
+								System.out.println(pageCount);						
+							}else{
+								System.out.println("No pages appeared");
+							}
+				}
+				
+			}
+				
+			
+				
+								
+				
+				
+			
+		public String recordCount(){
+			String recordCount = lblRecordCount.getText();
+			return recordCount;			
+		}
 		
 		
 	
